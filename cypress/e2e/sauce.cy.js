d8a08f90953507c396d776c5ca31b3fb9ded9ef4
dantarnacop <83305675+dantarnacop@users.noreply.github.com>
2023-03-06 13:48:23 +0200

    new locked out testcase

--- cypress/e2e/sauce.cy.js
+++ cypress/e2e/sauce.cy.js
@@ -1,8 +1,10 @@
 import selectors from '../support/sauceSelectors'
 
-describe('sauce automated tests example', () => {
+describe('sauce automated tests examples', () => {
   it('visits saucedemo home page', () => {
     cy.visit(Cypress.env('sauceUrl'))
+    cy.url().
+    should('eq', 'https://www.saucedemo.com/')
   })
   it('fails login with incorrect credentials', () => {
     cy.visit(Cypress.env('sauceUrl'));
@@ -20,5 +22,14 @@ describe('sauce automated tests example', () => {
     cy.get('#login-button').click()
     cy.url()
     .should('eq', 'https://www.saucedemo.com/inventory.html')
+  })
+  it('checks locked out user', () => {
+    cy.visit(Cypress.env('sauceUrl'))
+    cy.get(selectors.usernameId).type('locked_out_user')
+    cy.get(selectors.passwordId).type('secret_sauce')
+    cy.get(selectors.loginButtonId).click()
+    cy.get(selectors.loginErrorHeader)
+    .invoke('text')
+    .should('eq', "Epic sadface: Sorry, this user has been locked out.")
   })
 })
