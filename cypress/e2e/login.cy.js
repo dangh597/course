3d2c9e841ca8732a84928df8570887b89f1731f5
dantarnacop <83305675+dantarnacop@users.noreply.github.com>
2023-03-16 17:31:30 +0200

    yes yes yes

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -4,14 +4,14 @@ import errors from '../fixtures/errorMessages.json'
 describe('Login page tests', () => {
   it('logins succesfully', () => {
     cy.get(selectors.loginUsernameInput).type(Cypress.env('sauceUser'));
-    cy.get(selectors.loginPasswordInput).type(Cypress.env('SAUCEPASSWORD'), {log: false});
+    cy.get(selectors.loginPasswordInput).type(Cypress.env('saucePassword'), {log: false});
     cy.get(selectors.loginButton).click();
     cy.url()
       .should('eq', 'https://www.saucedemo.com/inventory.html');
   })
   it('fails login with incorrect credentials', () => {
     cy.get(selectors.loginUsernameInput).type(Cypress.env('sauceBadUser'));
-    cy.get(selectors.loginPasswordInput).type(Cypress.env('SAUCEPASSWORD'), {log: false});
+    cy.get(selectors.loginPasswordInput).type(Cypress.env('saucePassword'), {log: false});
     cy.get(selectors.loginButton).click();
     cy.get(selectors.loginErrorHeader)
       .invoke('text')
@@ -19,7 +19,7 @@ describe('Login page tests', () => {
   })
   it('fails login with locked user', () => {
     cy.get(selectors.loginUsernameInput).type(Cypress.env('sauceLockedUser'));
-    cy.get(selectors.loginPasswordInput).type(Cypress.env('SAUCEPASSWORD'), {log: false});
+    cy.get(selectors.loginPasswordInput).type(Cypress.env('saucePassword'), {log: false});
     cy.get(selectors.loginButton).click();
     cy.get(selectors.loginErrorHeader)
       .invoke('text')
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -4,7 +4,7 @@ Cypress.Commands.add('login', (user, password) => {
     cy.session([user, password], () => {
         cy.visit(Cypress.env("sauceUrl"))
         cy.get(selectors.loginUsernameInput).type(Cypress.env('sauceUser'), { log: false });
-        cy.get(selectors.loginPasswordInput).type(Cypress.env('SAUCEPASSWORD'), { log: false });
+        cy.get(selectors.loginPasswordInput).type(Cypress.env('saucePassword'), { log: false });
         cy.get(selectors.loginButton).click();
         cy.url().should('eq', 'https://www.saucedemo.com/inventory.html')
     })
