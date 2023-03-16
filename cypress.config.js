c8d3ece52e6efd4c51f54292c4237e3834d0558f
dantarnacop <83305675+dantarnacop@users.noreply.github.com>
2023-03-16 17:29:45 +0200

    yesyes

--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,8 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   "video": false,
   env: {
+    saucePassword: "",
     sauceUser: "standard_user",
-    saucePassword: "secret_sauce",
     sauceUrl: "https://www.saucedemo.com",
     sauceBadUser: "standard-user",
     sauceLockedUser: "locked_out_user",
