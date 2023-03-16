ca40b8ac2e06034cd0b11518cb23645eeda2bc7c
dantarnacop <83305675+dantarnacop@users.noreply.github.com>
2023-03-16 17:18:25 +0200

    secret test

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -3,23 +3,23 @@ import errors from '../fixtures/errorMessages.json'
 //test suite
 describe('Login page tests', () => {
   it('logins succesfully', () => {
-    cy.get(selectors.loginUsernameInput).type(Cypress.env('sauceUser'), {log: false});
-    cy.get(selectors.loginPasswordInput).type(Cypress.env('saucePassword'), {log: false});
+    cy.get(selectors.loginUsernameInput).type(Cypress.env('sauceUser'));
+    cy.get(selectors.loginPasswordInput).type(Cypress.env('SAUCEPASSWORD'), {log: false});
     cy.get(selectors.loginButton).click();
     cy.url()
       .should('eq', 'https://www.saucedemo.com/inventory.html');
   })
   it('fails login with incorrect credentials', () => {
-    cy.get(selectors.loginUsernameInput).type(Cypress.env('sauceBadUser'), {log: false});
-    cy.get(selectors.loginPasswordInput).type(Cypress.env('saucePassword'), {log: false});
+    cy.get(selectors.loginUsernameInput).type(Cypress.env('sauceBadUser'));
+    cy.get(selectors.loginPasswordInput).type(Cypress.env('SAUCEPASSWORD'), {log: false});
     cy.get(selectors.loginButton).click();
     cy.get(selectors.loginErrorHeader)
       .invoke('text')
       .should('eq', errors.invalidLoginError)
   })
   it('fails login with locked user', () => {
-    cy.get(selectors.loginUsernameInput).type(Cypress.env('sauceLockedUser'), {log: false});
-    cy.get(selectors.loginPasswordInput).type(Cypress.env('saucePassword'), {log: false});
+    cy.get(selectors.loginUsernameInput).type(Cypress.env('sauceLockedUser'));
+    cy.get(selectors.loginPasswordInput).type(Cypress.env('SAUCEPASSWORD'), {log: false});
     cy.get(selectors.loginButton).click();
     cy.get(selectors.loginErrorHeader)
       .invoke('text')
